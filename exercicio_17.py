f541b10408fbda855b0ef29ceecb22c09dad1197
Olivex13 <silva.o.douglas@gmail.com>
2024-01-04 16:59:52 -0300

    update arquivos dos exercicios

---
+++ exercicio_17.py
@@ -0,0 +1,22 @@
+#	Faça um Programa que peça um número correspondente a um determinado ano 
+# em seguida informe se este ano é ou não bissexto.
+
+print('==========ANO BISSEXTO=========='.center(60))
+print('')
+
+def eh_bissexto(ano):
+    if (ano % 4 == 0):
+        if (ano % 100 == 0):
+            if (ano % 400 == 0):
+                return 'É bissexto'
+            else: 
+                return 'não é bissexto'
+        else: 
+            return 'não é bissexto'
+    else:
+        return 'não é bissexto'
+    
+ano = int(input('Digite o ano: '))
+bissexto = eh_bissexto(ano)
+
+print(f'O ano {ano}, {bissexto}')
