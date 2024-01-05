8744a4111e83c66b5a8369c70328d2bef3f229a1
Douglas Silva <silva.o.douglas@gmail.com>
2024-01-05 00:06:19 -0300

    20,22,25,26

--- exercicio_20.py
+++ exercicio_20.py
@@ -3,3 +3,29 @@
 #- a.	A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
 #- b.	A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
 #- c.	A mensagem "Aprovado com Distinção", se a média for igual a 10.
+
+print("<==========CALCULADORA DE NOTAS ESCOLA DO PYTHON==========>".center(120))
+print('')
+
+def calculadora_media(n1, n2, n3):
+    media = (n1 + n2 + n3) / 3
+    return media
+
+def status_final(media):
+    if (media == 10):
+        status = "Aprovado com Distinção"
+    elif (media < 7):
+        status = "Reprovado"
+    else: 
+        status = "Aprovado"
+    
+    return status
+
+nota_1 = float(input('Digite a primeira nota: '))
+nota_2 = float(input('Digite a segunda nota: '))
+nota_3 = float(input('Digite a terceira nota: '))
+
+media_final = calculadora_media(nota_1, nota_2, nota_3)
+resultado = status_final(media_final)
+
+print(f'O resultado das duas notas é: {resultado}')
---
+++ exercicio_25.py
@@ -0,0 +1,73 @@
+#25.	Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+# a.	"Telefonou para a vítima?"
+# b.	"Esteve no local do crime?"
+# c.	"Mora perto da vítima?"
+# d.	"Devia para a vítima?"
+# e.	"Já trabalhou com a vítima?" O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. 
+# Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". 
+# Caso contrário, ele será classificado como "Inocente".
+
+print('<=====INVESTIGAÇÃO CRIMINAL======>'.center(60))
+print('')
+
+main = """Atenção, você esta sendo investigado.
+Responda as perguntas a seguir com:
+            SIM ou NÃO"""
+
+print(main)
+print("")
+
+pergunta1 = str(input('Voce telefonou para a vitima? => ')).lower()
+pergunta2 = str(input('Esteve no local do crime? => ')).lower()
+pergunta3 = str(input('Mora perto da Vitima? => ')).lower()
+pergunta4 = str(input('Devia para a vitima? => ')).lower()
+pergunta5 = str(input('Ja trabalhou com a vítima? => ')).lower()
+contagem = None
+
+def status(pergunta1, pergunta2, pergunta3, pergunta4, pergunta5):
+
+    resultado = 0
+
+    if (pergunta1 == 'sim'):
+        resultado += 1
+    else:
+        resultado += 0
+    
+    if (pergunta2 == 'sim'):
+        resultado += 1
+    else:
+        resultado += 0
+    
+    if (pergunta3 == 'sim'):
+        resultado += 1
+    else:
+        resultado += 0
+
+    if (pergunta4 == 'sim'):
+        resultado += 1
+    else:
+        resultado += 0
+    
+    if (pergunta5 == 'sim'):
+        resultado += 1
+    else:
+        resultado += 0
+
+    return resultado
+
+def apuracao(resultado):
+    if (resultado == 2):
+        return "Você é suspeito!"
+    elif (resultado >= 3 and resultado <=4):
+        return "Você é cúmplice!"
+    elif (resultado == 5):
+        return "Você é o assasino!"
+    else:
+        return "Você é inocente!"
+    
+contagem = status(pergunta1, pergunta2, pergunta3, pergunta4, pergunta5)
+
+veredito = apuracao(contagem)
+
+
+print(f'De acordo com o resultado da investigação, {veredito}')
