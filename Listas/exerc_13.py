157e598605e25e46d78a7bd3e2d0b1295cc9f171
Douglas <silva.o.douglas@gmail.com>
2024-08-21 01:17:17 -0300

    exerc 13-14

--- Listas/exerc_13.py
+++ Listas/exerc_13.py
@@ -2,3 +2,23 @@
 # Após isto, calcule a média anual das temperaturas e mostre todas as temperaturas acima da média anual,
 #  e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
 
+print('MEDIA DE TEMPERATURA'.center(60))
+print('-' * 60)
+print('')
+
+meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+
+mediaTemperatura = []
+
+for mes in meses:
+    temperatura = float(input(f'Digite a média de temperatura de {mes}: '))
+    mediaTemperatura.append(temperatura)
+
+mediaAnual = sum(mediaTemperatura) / 12
+
+print('')
+print(f'A média de temperatura anual foi {mediaAnual:.2f}')
+print('')
+for mes, temperatura in zip(meses, mediaTemperatura):
+    if temperatura > mediaAnual:
+        print(f'A temperatura em {mes} foi maior que a média anual atingindo {temperatura} graus.')
--- Listas/exerc_14.py
+++ Listas/exerc_14.py
@@ -13,5 +13,78 @@ As perguntas são:
            
            """
 
+print('Investigação Criminal'.center(60))
+print('-' * 60)
+print('')
+
+respostasPositivas = []
+
+print('Responda as perguntas a seguir com SIM ou NÃO: \n')
+
+while True:
+    perguntaA = input('Telefonou para a vítima?: ')
+    if perguntaA.lower() == "sim":
+        respostasPositivas.append("Telefonou para a vítima")
+        break
+    elif perguntaA.lower() == "nao" or perguntaA.lower() == "não":
+        break
+    else:
+        print("Responda com 'Sim' ou 'Não' ")
+
+while True:
+    perguntaB = input('Esteve no local do crime?: ')
+    if perguntaB.lower() == "sim":
+        respostasPositivas.append("Esteve no local do crime")
+        break
+    elif perguntaB.lower() == "nao" or perguntaB.lower() == "não":
+        break
+    else:
+        print("Responda com 'Sim' ou 'Não' ")
+
+while True:
+    perguntaC = input('Mora perto da vítima?: ')
+    if perguntaC.lower() == "sim":
+        respostasPositivas.append("Mora perto da vítima")
+        break
+    elif perguntaC.lower() == "nao" or perguntaC.lower() == "não":
+        break
+    else:
+        print("Responda com 'Sim' ou 'Não' ")
+
+while True:
+    perguntaD = input('Devia para a vítima?: ')
+    if perguntaD.lower() == "sim":
+        respostasPositivas.append("Devia para a vítima")
+        break
+    elif perguntaD.lower() == "nao" or perguntaD.lower() == "não":
+        break
+    else:
+        print("Responda com 'Sim' ou 'Não' ")
+
+while True:
+    perguntaE = input("Já trabalhou com a vítima?: ")
+    if perguntaE.lower() == "sim":
+        respostasPositivas.append("Esteve no local do crime")
+        break
+    elif perguntaE.lower() == "nao" or perguntaE.lower() == "não":
+        break
+    else:
+        print("Responda com 'Sim' ou 'Não' ")
 
 
+contador = 0
+for i in respostasPositivas:
+    contador += 1
+
+if contador == 2:
+    veredito = 'Suspeito'
+elif contador == 3 or contador == 4:
+    veredito = 'Cúmplice'
+elif contador == 5:
+    veredito = 'Assasino'
+else:
+    veredito = 'Inocente'
+
+
+print('')
+print(f'O resultado da investigação foi: \n {respostasPositivas} \n O individuo é: {veredito}')
