a73093b2cd2f3a96d05a1076a416beed2b55ada6
Olivex13 <silva.o.douglas@gmail.com>
2024-08-21 15:45:07 -0300

    exerc 15-16

--- Listas/exerc_15.py
+++ Listas/exerc_15.py
@@ -10,3 +10,58 @@
 - f.	Calcule e mostre a quantidade de valores acima da média calculada;
 - g.	Calcule e mostre a quantidade de valores abaixo de sete;
 - h.	Encerre o programa com uma mensagem;'''
+
+print('LENDO VALORES'.center(60))
+print('-' * 60)
+print('')
+
+
+valores = []
+
+print("Digite a quantidade de valores necessários, quando desejar encerrar, digite '-1'")
+
+while True:
+    valor = float(input('Digite um valor: '))
+    if valor == -1:
+        break
+    else:
+        valores.append(valor)
+
+valoresDigitados = 0
+
+for i in valores:
+    valoresDigitados += 1
+
+print('')
+print(f'Foram lidos {valoresDigitados} valores. \n')
+print(f'Os valores digitados foram: {valores} \n')
+print('Valores impressos em ordem inversa, um em baixo do outro. \n')
+
+for i in reversed(valores):
+    print(i)
+
+print('')
+somaValores = sum(valores)
+
+mediaValores = somaValores / valoresDigitados
+
+print(f'A soma dos valores digitados é: {somaValores} \n')
+print(f' A média dos valores digitados é: {mediaValores} \n')
+
+acimaDaMedia = []
+valoresAbaixo = []
+
+for valor in valores:
+    if valor > mediaValores:
+        acimaDaMedia.append(valor)
+    elif valor < 7:
+        valoresAbaixo.append(valor)
+
+print(f'Os valores acima da média digitados foram: \n {acimaDaMedia} \n')
+print(f'Os valores abaixo de 7 digitados foram: \n {valoresAbaixo} \n')
+print('Programa encerrado com sucesso.')
+
+
+
+
+
--- Listas/exerc_16.py
+++ Listas/exerc_16.py
@@ -17,3 +17,60 @@
 
 #Desafio: Crie ma fórmula para chegar na posição da lista a partir do salário, 
 #sem fazer vários ifs aninhados.
+
+print('Pagamento dos vendedores'.center(60))
+print('-' * 60)
+print('')
+
+contadores = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+salarioFixo = 200
+intervalos = [
+    "$200 - $299",
+    "$300 - $399",
+    "$400 - $499",
+    "$500 - $599",
+    "$600 - $699",
+    "$700 - $799",
+    "$800 - $899",
+    "$900 - $999",
+    "$1000 ou mais"
+]
+
+vendasBrutas = []
+contadorLoop = 1 
+
+print('Digite as vendas brutas dos vendedores ou Digite -1 para sair. \n')
+
+while True:
+    venda = float(input(f'Digite o valor da vendas Brutas do vendedor {contadorLoop}: '))
+    if venda == -1:
+        break
+    else:
+        vendasBrutas.append(venda)
+        contadorLoop += 1
+
+salarioTotal = []
+
+for vendaBruta in vendasBrutas:
+    salario = salarioFixo + (vendaBruta * 0.09)
+    salarioTotal.append(salario)
+  
+    if salario >= 1000:
+        indice = 8  
+    else:
+        indice = int((salario - salarioFixo) // 100)
+
+        contadores[indice] += 1
+
+
+print('\nResultados:')
+for i in range(len(contadores)):    
+    print(f"{intervalos[i]}: {contadores[i]} vendedor(es)")
+
+print('')
+print('Salário dos Vendedores: \n')
+contSalario = 1
+
+for i in salarioTotal:
+    print(f'O vendedor {contSalario} ganhou ${i}.')
+    contSalario += 1
